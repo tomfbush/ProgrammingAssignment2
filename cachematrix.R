fb02daa24bec7cc792091769d5d9cb5c051fb43a
Tom Bush <tomfbush@gmail.com>
2015-02-22 11:45:34 +0000

    Better comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions enable the caching of a matrix and its inversion for 
+## efficient retrieval, in the event that the inversion of the same input matrix
+## is requested a second time. The functions exploit the use of R's lexical
+## scoping and the '<<-' operator to enable the passing of values that would
+## normally be out of scope for the environments of the functions.
 
-## Write a short comment describing this function
+## makeCacheMatrix is a special function that contains a list of other functions
+## that enable the setting and retrieving of the source matrix, as well as the
+## setting and retrieving of the inverted. The actual inverting is done in the
+## cacheSolve function.
+
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -16,7 +23,10 @@ makeCacheMatrix <- function(x = matrix()) {
              getinverted = getinverted)
 }
 
-## Write a short comment describing this function
+## cacheSolve checks if m is null; if it is not, it retrieves the cached,
+## inverted matrix. If it is, it retrieves the uninverted matrix from its
+## argument, passes it to data, uses the solve() function to invert it and sets
+## it back with the special function x$setinverted(). Finally it returns it.
 cacheSolve <- function(x, ...) {
         m <- x$getinverted()
         if(!is.null(m)) {
